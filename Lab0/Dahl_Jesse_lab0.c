a29ea2ef5758ec9de7432bce08ac30e7e8e22dd6
jesse-dahl <40815531+jesse-dahl@users.noreply.github.com>
2018-09-16 17:30:19 -0600

    Add files via upload

---
+++ Lab0/Dahl_Jesse_lab0.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/* Jesse Dahl
+ *Lab 0, CSCI112
+ *8/28/18
+ */
+
+int main(void) {
+  printf("Hello World\n");
+  //prints "Hello World"
+  return(0);
+  //Exit with no errors
+}
